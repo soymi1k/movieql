913a0570ce3825caccf9085e75930e8cb2ba60ff
soymi1k <soymilkneigh@gmail.com>
2021-02-05 16:34:44 +0900

    0.6 Extending the Schema

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./graphql/resolvers";
 
-const server = new GraphQLServer({});
+const server = new GraphQLServer({
+  typeDefs: "graphql/schema.graphql", //type의 정의
+  resolvers, // = resolvers: resolbers
+});
 
 server.start(() => console.log("Graphql Server Running"));
