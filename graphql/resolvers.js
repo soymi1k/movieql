f322ada4e8518eb0f8a03e58ec2e0e9305cc3d72
soymi1k <soymilkneigh@gmail.com>
2021-02-08 17:11:11 +0900

    resolvers data up

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,12 +1,51 @@
-const nicolas = {
-  name: "Nicolas",
-  age: 18,
-  gender: "female",
-};
+const people = [
+  {
+    id: "0",
+    name: "Nicolas",
+    age: 18,
+    gender: "female",
+  },
+  {
+    id: "1",
+    name: "Jisu",
+    age: 18,
+    gender: "female",
+  },
+  {
+    id: "2",
+    name: "Japan Guy",
+    age: 18,
+    gender: "male",
+  },
+  {
+    id: "3",
+    name: "Daal",
+    age: 18,
+    gender: "male",
+  },
+  {
+    id: "4",
+    name: "JD",
+    age: 18,
+    gender: "male",
+  },
+  {
+    id: "5",
+    name: "moondaddi",
+    age: 18,
+    gender: "male",
+  },
+  {
+    id: "6",
+    name: "flynn",
+    age: 18,
+    gender: "male",
+  },
+];
 
 const resolvers = {
   Query: {
-    person: () => nicolas,
+    people: () => people,
   },
 };
 
